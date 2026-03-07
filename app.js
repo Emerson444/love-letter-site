75dadfcbf0612bfb7839b694282231a972e96a76
Emerson444 <132405708+Emerson444@users.noreply.github.com>
2026-03-07 11:34:11 -0300

    atualização de musicas

--- app.js
+++ app.js
@@ -17,17 +17,12 @@ function atualizarContador() {
     document.getElementById("contador").innerHTML =
         `${anos} anos, ${meses} meses e ${dias} dias`;
 }
-
 atualizarContador();
-
-
-
 const linhas = [
 "Meu amor, feliz dia das mulheres. Você é uma mulher incrível, forte, linda e que faz meus dias muito mais felizes só por estar na minha vida.",
 "Sou muito grato por ter você comigo. Seu jeito, seu carinho e sua companhia significam muito pra mim.",
 "Feliz Dia das Mulheres, meu amor. Te amo demais, Mira ❤️"
 ];
-
 let linhaIndex = 0;
 let letraIndex = 0;
 
@@ -49,31 +44,24 @@ function escrever() {
 
             setTimeout(escrever, 800);
         }
-
     }
 }
-
 escrever();
-
 let playlist = [
-  "assets/audios/music1.mp3",
-  "assets/audios/music2.mp3",
-  "assets/audios/music3.mp3"
+  "assets/audios/audio1.mp3",
+  "assets/audios/audio2.mp3",
+  "assets/audios/audio3.mp3"
 ];
-
 let indice = 0;
-
 let player = document.getElementById("player");
 let playBtn = document.getElementById("playBtn");
 let nextBtn = document.getElementById("nextBtn");
-
 function tocar(){
   player.src = playlist[indice];
   player.play();
 }
 
 playBtn.addEventListener("click", function(){
-
   if(player.paused){
     tocar();
     playBtn.innerHTML = "⏸";
@@ -81,29 +69,18 @@ playBtn.addEventListener("click", function(){
     player.pause();
     playBtn.innerHTML = "▶️";
   }
-
 });
-
 nextBtn.addEventListener("click", function(){
-
   indice++;
-
   if(indice >= playlist.length){
     indice = 0;
   }
-
   tocar();
-
 });
-
 player.addEventListener("ended", function(){
-
   indice++;
-
   if(indice >= playlist.length){
     indice = 0;
   }
-
   tocar();
-
 });
